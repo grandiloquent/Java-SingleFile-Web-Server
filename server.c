4ca2be2a664649b7b75e99bf892e160fb1dd0cc6
grandiloquent <1785907865@qq.com>
2019-01-07 19:18:18 +0800

    changed

--- server.c
+++ server.c
@@ -5,7 +5,60 @@
 #endif
 
 #include <stdio.h>
+#include <ctype.h>
+#include <dirent.h>
+#include <sys/stat.h>
 
+#define BUFFER_SIZE 1024
+#define BUFFER_SIZE_METHOD 10
+
+void accpet_request(int client);
+void not_found(int client);
+int startup(u_short *port, const char *ip);
+
+int get_line(int client, char *buf, int size) {
+  int i = 0;
+  char c = '\0';
+  int n;
+  while ((i < size - 1) && (c != '\n')) {
+    // https://docs.microsoft.com/en-us/windows/desktop/api/winsock/nf-winsock-recv
+    n = recv(client, &c, 1, 0);
+    if (n > 0) {
+      if (c == '\r') {
+        n = recv(client, &c, 1, MSG_PEEK);
+        if ((n > 0) && (c == '\n'))
+          recv(client, &c, 1, 0);
+        else
+          c = '\n';
+      }
+      buf[i] = c;
+      i++;
+    } else
+      c = '\n';
+  }
+  buf[i] = '\0';
+  return i;
+}
+void accpet_request(int client) {
+  char buf[BUFFER_SIZE];
+  int numchars;
+
+  numchars = get_line(client, buf, BUFFER_SIZE);
+
+  size_t i = 0, j = 0;
+  char method[BUFFER_SIZE_METHOD];
+  while (!isspace(buf[j]) && (i < BUFFER_SIZE_METHOD - 1)) {
+    method[i] = buf[j];
+    i++;
+    j++;
+  }
+  method[i] = '\0';
+  if (strcasecmp(method, "GET") && strcasecmp(method, "POST")) {
+    printf("Dont support the method = %s.\n", method);
+    return;
+  }
+  printf("%s\n", method);
+}
 void not_found(int client) {}
 int startup(u_short *port, const char *ip) {
 #ifdef _WIN32
@@ -15,7 +68,6 @@ int startup(u_short *port, const char *ip) {
     printf("[Failed]: WSAStartup. result = %d.\n", result);
     return -1;
   }
-
 #endif
   // https://docs.microsoft.com/en-us/windows/desktop/api/winsock/ns-winsock-sockaddr_in
   struct sockaddr_in name;
@@ -39,9 +91,7 @@ int startup(u_short *port, const char *ip) {
     return -1;
   }
   //   getsockname(httpd, (struct sockaddr *)&name, &addrlen); // read binding
-
   //   unsigned short local_port = ntohs(name.sin_port);
-
   //   char *ip = inet_ntoa(name.sin_addr);
   //   printf("ip: %s, port: %d\n", ip, local_port);
   if (*port == 0) {
@@ -51,32 +101,79 @@ int startup(u_short *port, const char *ip) {
     }
     *port = ntohs(name.sin_port);
   }
-
   if (listen(httpd, 5) < 0) {
     puts("[Failed]: listen");
     return httpd;
   }
   return httpd;
 }
-void accpet_request(int client) {}
-int main(int argc, char *argv[]) {
-  u_short port = 8090;
-  int server = startup(&port, "127.0.0.1");
-  int client = -1;
-  struct sockaddr_in client_name;
-  int client_name_len = sizeof(client_name);
+typedef struct _File {
+  char *path;
+  size_t time;
+} File;
 
-  while (1) {
-    // https://docs.microsoft.com/en-us/windows/desktop/api/winsock/ns-winsock-sockaddr
-    client = accept(server, (struct sockaddr *)&client_name, &client_name_len);
-    puts("accept");
-    if (client == -1) {
-      accpet_request(client);
+File *list_files(const char *path) {
+  DIR *dir;
+  struct dirent *ent;
+  File *files = malloc(100 * sizeof(File));
+  int index = 0;
+  struct stat s;
+
+  if ((dir = opendir(path)) != NULL) {
+    while ((ent = readdir(dir)) != NULL) {
+      if (strcmp(ent->d_name, ".") == 0 || strcmp(ent->d_name, "..") == 0)
+        continue;
+      char p[MAX_PATH];
+      strcpy(p, path);
+      strcat(p, "/");
+      strcat(p, ent->d_name);
+
+      stat(p, &s);
+      if (S_ISDIR(s.st_mode)) {
+        list_files(p);
+      } else {
+        if (index < 100) {
+          File file;
+          char *buf = malloc(MAX_PATH * sizeof(char));
+          strcpy(buf, p);
+          file.path = buf;
+          file.time = 123;
+          files[index++] = file;
+        } else {
+          break;
+        }
+      }
     }
+
+    closedir(dir);
   }
-  // close(server);
-#ifdef _WIN32
-  WSACleanup();
-#endif
-  return 0;
+  return files;
+}
+
+int main(int argc, char *argv[]) {
+  File *files = list_files("C:\\Users\\psycho\\Downloads");
+  printf("%d\n", sizeof(files));
+  int index = 0;
+  while (index++ < 100) {
+    printf("%s\n", files->path);
+    files++;
+  }
+  //   u_short port = 8090;
+  //   int server = startup(&port, "127.0.0.1");
+  //   int client = -1;
+  //   struct sockaddr_in client_name;
+  //   int client_name_len = sizeof(client_name);
+  //   while (1) {
+  //     //
+  //     https://docs.microsoft.com/en-us/windows/desktop/api/winsock/ns-winsock-sockaddr
+  //     client = accept(server, (struct sockaddr *)&client_name,
+  //     &client_name_len); puts("accept"); if (client == -1) {
+  //     }
+  //     accpet_request(client);
+  //   }
+  //   // close(server);
+  // #ifdef _WIN32
+  //   WSACleanup();
+  // #endif
+  //   return 0;
 }
